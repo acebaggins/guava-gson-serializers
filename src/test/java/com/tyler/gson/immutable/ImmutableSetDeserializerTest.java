c86ea41b17d845e30fe397da2d5b574c5e172775
rniesler <rniesler@google.com>
2019-03-05 15:20:07 +0100

    Fix deserialization of classes contained in a container
    Move construction of TypeParameter to corresponding method to allow
    proper capture of the type parameter context.
    
    Fixes #4

--- src/test/java/com/tyler/gson/immutable/ImmutableSetDeserializerTest.java
+++ src/test/java/com/tyler/gson/immutable/ImmutableSetDeserializerTest.java
@@ -1,7 +1,7 @@
 package com.tyler.gson.immutable;
 
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.util.Set;
@@ -10,59 +10,64 @@ import org.hamcrest.core.Is;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class ImmutableSetDeserializerTest {
 	private final ImmutableSet<String> testSet = ImmutableSet.of( "a", "b", "c" );
 	private final String json = "{ \"set\" : [\"a\", \"b\", \"c\" ] }";
-	
+
 	@Test
-	public void testDeserializeImmutableInterface_1() {
+	public void testDeserializeImmutableInterface() {
 		final Gson gson = new GsonBuilder().registerTypeAdapter(ImmutableSet.class, new ImmutableSetDeserializer()).create();
 		final ImmutableInterface tester = gson.fromJson( json, ImmutableInterface.class );
-		
-		assertThat( tester.set, Is.is( testSet ));		
+
+		assertThat( tester.set, Is.is( testSet ));
 	}
-	
-	@Test( expected = ClassCastException.class )
-	public void testDeserializeImmutableInterface_2() {
-		final Gson gson = new GsonBuilder().registerTypeAdapter(ImmutableSet.class, new ImmutableSetDeserializer()).create();
-		ImmutableInterface tester = gson.fromJson( "{ \"set\" : [1,2,3] }", ImmutableInterface.class );	
-		
-		assertNotNull( Iterables.get( tester.set, 0).getClass() );
-	}
-	
+
 	@Test
-	public void testDeserializeImplemntation_1() {
+	public void testDeserializeImplementation() {
 		final Gson gson = new GsonBuilder().registerTypeAdapter(Set.class, new ImmutableSetDeserializer()).create();
 		final ImmutableImplementation tester = gson.fromJson( json, ImmutableImplementation.class );
-		
-		assertThat( tester.set, Is.is( (Set<String>)testSet ));		
+
+		assertThat( tester.set, Is.is( (Set<String>)testSet ));
 	}
-	
-	@Test( expected = ClassCastException.class )
-	public void testDeserializeImmutableImplemntation_2() {
+
+	@Test
+	public void testDeserializeNested() {
 		final Gson gson = new GsonBuilder().registerTypeAdapter(Set.class, new ImmutableSetDeserializer()).create();
-		ImmutableImplementation tester = gson.fromJson( "{ \"set\" : [1,2,3] }", ImmutableImplementation.class );
-		
-		assertNotNull( Iterables.get( tester.set, 0).getClass() );
+		final Parent tester = gson.fromJson("{'set': [{'name':'nested'}]}",Parent.class);
+
+		assertEquals(tester.set.iterator().next().name, "nested");
 	}
-		
-	private static final class ImmutableInterface { 
+
+	private static final class ImmutableInterface {
 		private final ImmutableSet<String> set;
-		
+
 		private ImmutableInterface(){
 			this.set = ImmutableSet.of();
-		}		
+		}
 	}
-	
-	private static final class ImmutableImplementation { 
+
+	private static final class ImmutableImplementation {
 		private final Set<String> set;
-		
+
 		private ImmutableImplementation(){
 			this.set = ImmutableSet.of();
-		}		
+		}
+	}
+
+	private static class Parent {
+		private Set<Nested> set;
+
+		private static class Nested {
+			private String name;
+
+			public boolean equals(Object other) {
+				if(!(other instanceof Nested))
+					return false;
+				return name.equals(((Nested) other).name);
+			}
+		}
 	}
 }
